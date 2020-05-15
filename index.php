00721a62536c9d041fb2c6731e5d4b9b38f58d4f
dtakada <57592950+dtakada@users.noreply.github.com>
2020-05-15 14:52:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php
-
-echo "Hello World! This is my Index page!";
-
-?>
+<?php require "header.php"; ?>
+      <div class="container-fluid">
+        <h1 class="mt-4">
+          <main>
+            <Center>
+            <br/><br/>
+            <p>You are logged in!</p>
+            <p>You are logged out!</p>
+                  <?php
+                  // nothing here
+                  ?>
+            </Center>
+          </main>
+        </h1>
+        <p>text 1</p>
+        <p><code>text 2</code></p>
+      </div>
+  <?php require "footer.php"; ?>
